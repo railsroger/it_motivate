d9d4772e6152a6d00de1b3119d847ceec6a10f5f
railsroger <railsroger@gmail.com>
2023-12-19 12:17:10 +0100

    Adding a Post Destroy Action and Refactoring

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,45 +1,52 @@
 class PostsController < ApplicationController
+    before_action :set_post, except: [:index, :new, :create]
     
     def index
         @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-        redirect_to root_path
     end
-
-    def new
-        @post = Post.new
+  
+    def new 
+      @post = Post.new
     end
-
+  
     def create
-        @post = Post.new(post_params)
-        if @post.save
-          redirect_to @post
-        else
-          render :new, status: :unprocessable_entity
-        end
+      @post = Post.new(post_params)
+      if @post.save
+        redirect_to @post
+      else
+        render :new, status: :unprocessable_entity
+      end
     end
-
+  
     def edit
-        @post = Post.find(params[:id])
     end
-    
+  
     def update
-      @post = Post.find(params[:id])
       if @post.update(post_params)
         redirect_to @post
       else
         render :edit, status: :unprocessable_entity
       end
     end
-    
+  
+    def destroy
+      @post.destroy
+      redirect_to root_path
+    end
+  
     private
-    
+  
     def post_params
       params.require(:post).permit(:title, :body)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
     end
     
 end
