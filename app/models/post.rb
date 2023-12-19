1fe63c7f47f970d35eb6ffd9fc356368c203387e
railsroger <railsroger@gmail.com>
2023-12-19 15:24:39 +0100

    Add published and add fix form

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,13 @@
 class Post < ApplicationRecord
     validates :title, presence: true
     validates :body, presence: true
+
+    scope :sorted, -> { order(published_at: :desc, updated_at: :desc) }
+    scope :published, -> { where("published_at <= ?", Time.current) }
+    
+
+    def published?
+        published_at? && published_at <= Time.current
+    end
+
 end
