05d1303f8f115f813096c3dcfc5707af7b1bf2cd
railsroger <railsroger@gmail.com>
2023-12-19 12:06:23 +0100

    Edit & Update Post Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,6 +22,19 @@ class PostsController < ApplicationController
           render :new, status: :unprocessable_entity
         end
     end
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+    
+    def update
+      @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to @post
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
     
     private
     
