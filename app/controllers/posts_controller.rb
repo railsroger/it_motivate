58eb529d7a3f2f502725c1b9432ad165d679367f
railsroger <railsroger@gmail.com>
2023-12-19 11:55:19 +0100

    Adding a New Post Action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,17 @@
 class PostsController < ApplicationController
+    
     def index
         @posts = Post.all
     end
+
+    def show
+        @post = Post.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+        redirect_to root_path
+    end
+
+    def new
+        @post = Post.new
+    end
+    
 end
